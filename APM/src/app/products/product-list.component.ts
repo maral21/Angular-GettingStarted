be81cc559bf61c677d9296d621d631a2c6a69bc2
Mar Andrés <mar.andres.l@gmv.com>
2021-03-08 17:22:17 +0100

    'Http Service'

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit{
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string;
 
     private _listFilter: string = 'cart';
     public get listFilter(): string {
@@ -38,8 +39,12 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.productService.getProducts().subscribe({
+         next: products => {this.products = products;
+            this.filteredProducts = this.products;},
+         error: err => this.errorMessage = err   
+        });
+        
     }
 
     onRatingClicked(message: string): void{
